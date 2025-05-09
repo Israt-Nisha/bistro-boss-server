23e9374e4b505e96c746ab12258d9e19b41b6ca5
Israt-Nisha <isratnisha1999@gmail.com>
2025-05-09 10:33:19 +0600

    add to cart with cartCollection

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
+    const cartCollection = client.db("bistroDb").collection("carts");
     
     app.get('/menu', async(req, res) => {
         const result =await menuCollection.find().toArray();
@@ -36,6 +37,18 @@ async function run() {
     app.get('/reviews', async(req, res) => {
         const result =await reviewCollection.find().toArray();
         res.send(result);
+    })
+
+    // carts collection
+    app.get('/carts', async (req, res) => {
+      const result = await cartCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
     })
 
     // Send a ping to confirm a successful connection
@@ -57,4 +70,3 @@ app.listen(port, () => {
     console.log(`Bistro boss is sitting on port ${port}`);
 })
 
-7
