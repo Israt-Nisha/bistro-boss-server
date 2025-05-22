dabae801693cc77e12b0792c04510f013c651e0c
Israt-Nisha <isratnisha1999@gmail.com>
2025-05-22 14:33:55 +0600

    payment added

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
+    const paymentCollection = client.db("bistroDb").collection("payments");
 
     // jwt related api
     app.post('/jwt', async (req, res) => {
@@ -45,28 +46,28 @@ async function run() {
     // middlewars
     const veryfyToken = (req, res, next) => {
       // console.log('inside verify token', req.headers.authorization);
-      if(!req.headers.authorization){
-        return res.status(401).send({ message: 'unauthorized access'})
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' })
       }
       const token = req.headers.authorization.split(' ')[1];
       jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if(err){
-          return res.status(401).send({ message: 'unauthorized access'})
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
         }
         req.decoded = decoded;
         next();
       })
 
-      
+
     }
-// use verify admin after verifytoken
+    // use verify admin after verifytoken
     const verifyAdmin = async (req, res, next) => {
-      const email= req.decoded.email;
-      const query = {email: email};
+      const email = req.decoded.email;
+      const query = { email: email };
       const user = await userCollection.findOne(query);
       const isAdmin = user?.role === 'admin';
-      if(!isAdmin){
-        return res.status(403).send({ message: 'forbidden access'});
+      if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
       }
       next();
     }
@@ -74,23 +75,23 @@ async function run() {
     // users related api
 
     app.get('/users', veryfyToken, verifyAdmin, async (req, res) => {
-      
+
       const result = await userCollection.find().toArray();
       res.send(result);
     });
 
-    app.get('/users/admin/:email', veryfyToken, async(req, res) =>{
+    app.get('/users/admin/:email', veryfyToken, async (req, res) => {
       const email = req.params.email;
-      if(email !== req.decoded.email) {
-        return res.status(403).send({message: 'forbidden access'})
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
       }
-      const query = {email: email};
-      const user =await userCollection.findOne(query);
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
       let admin = false;
-      if(user) {
+      if (user) {
         admin = user?.role === 'admin';
       }
-      res.send({admin});
+      res.send({ admin });
     })
 
     app.post('/users', async (req, res) => {
@@ -105,7 +106,7 @@ async function run() {
       res.send(result);
     });
 
-    app.patch('/users/admin/:id',veryfyToken, verifyAdmin, async (req, res) => {
+    app.patch('/users/admin/:id', veryfyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
@@ -117,7 +118,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete('/users/:id',veryfyToken, verifyAdmin, async (req, res) => {
+    app.delete('/users/:id', veryfyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
@@ -134,43 +135,43 @@ async function run() {
     });
 
 
-    app.post('/menu',veryfyToken, verifyAdmin, async(req, res) => {
+    app.post('/menu', veryfyToken, verifyAdmin, async (req, res) => {
       const item = req.body;
       const result = await menuCollection.insertOne(item);
       res.send(result);
     })
 
-    app.delete('/menu/:id', veryfyToken, verifyAdmin, async(req, res) => {
+    app.delete('/menu/:id', veryfyToken, verifyAdmin, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await menuCollection.deleteOne(query);
       res.send(result);
     })
 
     app.get('/menu/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await menuCollection.findOne(query);
       res.send(result);
     })
 
-  app.patch('/menu/:id',veryfyToken, verifyAdmin, async (req, res) => {
-    const item = req.body;
-    const id = req.params.id;
-    const filter = {_id: new ObjectId(id)};
-    const updatedDoc = {
-      $set: {
-        name: item.name,
-        category: item.category,
-        price: item.price,
-        recipe: item.recipe,
-        image: item.image
+    app.patch('/menu/:id', veryfyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image
 
+        }
       }
-    }
-    const result = await menuCollection.updateOne(filter, updatedDoc);
-    res.send(result);
-  })
+      const result = await menuCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
 
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
@@ -199,9 +200,9 @@ async function run() {
 
     // payment intent
     app.post('/create-payment-intent', async (req, res) => {
-      const {price} = req.body;
+      const { price } = req.body;
       const amount = parseInt(price * 100);
-      console.log('payment intent', amount)
+      // console.log('payment intent', amount)
 
       const paymentIntent = await stripe.paymentIntents.create({
         amount: amount,
@@ -211,6 +212,32 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret,
       })
+    });
+
+    app.get('/payments/:email', veryfyToken, async (req, res) => {
+      const query = {email: req.params.email}
+      if(req.params.email !== req.decoded.email){
+        return res.status(403).send({message: 'forbidden access'});
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/payments', async (req, res) => {
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      // carefully delete each item from the cart
+      console.log('payment info', payment);
+      const query = {
+        _id: {
+          $in: payment.cartIds.map(id => new ObjectId(id))
+        }
+      }
+
+      const deleteResult = await cartCollection.deleteMany(query);
+
+      res.send({ paymentResult, deleteResult });
     })
 
     // Send a ping to confirm a successful connection
